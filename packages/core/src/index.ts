ea546e0180e09f55e62a639d81bd35956718a575
Thacio <thacio@gmail.com>
2025-08-17 01:37:17 -0300

    fix: Re-add export for shellExecutionService in index.ts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -46,6 +46,7 @@ export * from './utils/errorParsing.js';
 // Export services
 export * from './services/fileDiscoveryService.js';
 export * from './services/gitService.js';
+export * from './services/shellExecutionService.js';
 
 // Export telemetry
 export { logIdeConnection } from './telemetry/loggers.js';
